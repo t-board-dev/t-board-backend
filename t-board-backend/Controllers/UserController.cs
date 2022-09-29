6426dfb4c2a5b3dd7f201093ba1cee31a0ee89e8
mbsagin <mbsagin@gmail.com>
2022-09-29 23:50:13 +0300

    UserController template

---
+++ t-board-backend/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+using System.Threading.Tasks;
+
+namespace t_board_backend.Controllers
+{
+    public class UserController : Controller
+    {
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login()
+        {
+            return new JsonResult(true);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser()
+        {
+            return new JsonResult(true);
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return new JsonResult(true);
+        }
+    }
+}
