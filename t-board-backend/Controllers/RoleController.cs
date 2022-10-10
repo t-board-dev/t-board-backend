9796c4e6972b5eca675dd0bb13a5807f95733309
mbsagin <mbsagin@gmail.com>
2022-10-11 00:28:53 +0300

    RoleController init

---
+++ t-board-backend/Controllers/RoleController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using t_board.Entity;
+using t_board_backend.Models.Role;
+
+namespace t_board_backend.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("role/")]
+    public class RoleController : Controller
+    {
+        private readonly UserManager<TBoardUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly TBoardDbContext _dbContext;
+        public RoleController(
+            UserManager<TBoardUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            TBoardDbContext dbContext)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("getAllRoles")]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            return Ok(_roleManager.Roles?.Select(r => r.NormalizedName).ToArray() ?? Array.Empty<string>());
+        }
+
+        [HttpPost("getUserRoles")]
+        public async Task<IActionResult> GetUserRoles([FromBody] GetUserRolesRequest getUserRolesRequest)
+        {
+            var user = await _userManager.FindByEmailAsync(getUserRolesRequest.Email);
+            if (user == null) return NotFound(getUserRolesRequest.Email);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            return Ok(JsonSerializer.Serialize(roles));
+        }
+    }
+}
