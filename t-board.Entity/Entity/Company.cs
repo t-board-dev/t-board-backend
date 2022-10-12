ad3e787bab1bd7ffb61ffc0b20baaff7a03a64cd
mbsagin <mbsagin@gmail.com>
2022-10-12 23:06:59 +0300

    Company and type entity

---
+++ t-board.Entity/Entity/Company.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace t_board.Entity.Entity
+{
+    public class Company
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Url { get; set; }
+    }
+}
