5306123c93b420c8a0d719bc0f2dc7f845c7afc7
mbsagin <mbsagin@gmail.com>
2022-10-19 23:52:07 +0300

    Brand & BrandUser entity

---
+++ t-board.Entity/Entity/Brand.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace t_board.Entity
+{
+    public class Brand
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Keywords { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public virtual Company Company { get; set; }
+    }
+}
---
+++ t-board.Entity/Entity/BrandUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace t_board.Entity
+{
+    public class BrandUser
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int BrandId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
