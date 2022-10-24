11a48f9cb2d1b9b365c8fd8ed5069fd72d519719
mbsagin <mbsagin@gmail.com>
2022-10-24 22:53:54 +0300

    UserService

---
+++ t-board.Services/Contracts/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using t_board.Entity;
+
+namespace t_board.Services.Contracts
+{
+    public interface IUserService
+    {
+        Task<(bool Succeeded, string Message)> CreateUser(TBoardUser user);
+        Task<(bool Succeded, string Message)> AssignUserRole(string userEmail, string role);
+    }
+}
---
+++ t-board.Services/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using t_board.Entity;
+using t_board.Services.Contracts;
+
+namespace t_board.Services.Services
+{
+    public sealed class UserService : IUserService
+    {
+        private readonly UserManager<TBoardUser> _userManager;
+
+        private readonly TBoardDbContext _dbContext;
+
+        public UserService(
+            UserManager<TBoardUser> userManager,
+            TBoardDbContext dbcontext)
+        {
+            _userManager = userManager;
+
+            _dbContext = dbcontext;
+        }
+
+        public async Task<(bool Succeeded, string Message)> CreateUser(TBoardUser user)
+        {
+            var created = await _userManager.CreateAsync(user);
+            return (created.Succeeded, string.Join(", ", created.Errors));
+        }
+
+        public async Task<(bool Succeded, string Message)> AssignUserRole(string userEmail, string role)
+        {
+            var user = await _userManager.FindByNameAsync(userEmail);
+            if (user == null) return (false, $"User '{userEmail}' could not found!");
+
+            var created = await _userManager.AddToRoleAsync(user, role);
+            return (created.Succeeded, string.Join(", ", created.Errors));
+        }
+    }
+}
