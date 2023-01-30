ad09d077e16a18721544763c6f80c2e3b7227476
mbsagin <mbsagin@gmail.com>
2023-01-30 18:23:29 +0300

    Scrapper controller init

---
+++ t-board-backend/Controllers/ScrapperController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using t_board.Services.Contracts;
+
+namespace t_board_backend.Controllers
+{
+    [Authorize]
+    public class ScrapperController : Controller
+    {
+        private readonly IScrapper _ajansPressScrapper;
+        private readonly IScrapper _interPressScrapper;
+        private readonly IScrapper _medyaTakipScrapper;
+
+        public ScrapperController(ServiceResolver serviceResolver)
+        {
+            _ajansPressScrapper = serviceResolver("AjansPress");
+            _interPressScrapper = serviceResolver("InterPress");
+            _medyaTakipScrapper = serviceResolver("MedyaTakip");
+        }
+    }
+}
