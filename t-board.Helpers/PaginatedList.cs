d535adc39ef81102156bdacb4a3d103a69c29715
mbsagin <mbsagin@gmail.com>
2023-02-07 17:34:38 +0300

    PaginatedList class for query pagination

---
+++ t-board.Helpers/PaginatedList.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace t_board.Helpers
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+
+        public int TotalPages { get; private set; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> query, int pageIndex = 1, int pageSize = 10)
+        {
+            var count = await query.CountAsync();
+
+            var items = await query.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
