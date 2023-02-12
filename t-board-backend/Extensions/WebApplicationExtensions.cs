674eee08c4b8d724790c959ee45b9cdabc3cacc3
mbsagin <mbsagin@gmail.com>
2023-02-12 12:57:58 +0300

    Exception middleware

---
+++ t-board-backend/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using t_board_backend.Middlewares;
+
+namespace t_board_backend.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void ConfigureCustomExceptionMiddleware(this WebApplication app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
---
+++ t-board-backend/Models/BaseResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace t_board_backend.Models
+{
+    public class BaseResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
