b2db78df08c7c1b56020b86b0388758a5c29e8b8
mbsagin <mbsagin@gmail.com>
2023-02-14 23:14:01 +0300

    SaveBrandFile endpoint

---
+++ t-board-backend/Models/Brand/SaveBrandFileRequest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace t_board_backend.Models.Brand
+{
+    public class SaveBrandFileRequest
+    {
+        [Required]
+        [JsonProperty("brandId")]
+        public int BrandId { get; set; }
+        [Required]
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [Required]
+        [JsonProperty("url")]
+        public string URL { get; set; }
+    }
+}
