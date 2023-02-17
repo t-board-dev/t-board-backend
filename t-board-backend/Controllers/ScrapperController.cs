ea52cddfd015bb7fd70ea080cb9ac2950f32aec0
mbsagin <mbsagin@gmail.com>
2023-02-17 16:58:42 +0300

    JsonSerializtion fix for scrapper

--- t-board-backend/Controllers/ScrapperController.cs
+++ t-board-backend/Controllers/ScrapperController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using t_board.Services.Contracts;
 
@@ -22,31 +24,46 @@ namespace t_board_backend.Controllers
         }
 
         [HttpPost("scrapeAjansPress")]
+        [Produces("application/json")]
         public async Task<IActionResult> ScrapeAjansPress(string url)
         {
             object scrappedModel = _ajansPressScrapper.Scrape(url);
 
-            var jsonModel = JsonConvert.SerializeObject(scrappedModel);
+            var jsonModel = JsonSerializer.Serialize(scrappedModel,
+                new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+                });
 
             return Ok(jsonModel);
         }
 
         [HttpPost("scrapeInterPress")]
+        [Produces("application/json")]
         public async Task<IActionResult> ScrapeInterPress(string url)
         {
             object scrappedModel = _interPressScrapper.Scrape(url);
 
-            var jsonModel = JsonConvert.SerializeObject(scrappedModel);
+            var jsonModel = JsonSerializer.Serialize(scrappedModel,
+                new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+                });
 
             return Ok(jsonModel);
         }
 
         [HttpPost("scrapeMedyaTakip")]
+        [Produces("application/json")]
         public async Task<IActionResult> ScrapeMedyaTakip(string url)
         {
             object scrappedModel = _medyaTakipScrapper.Scrape(url);
 
-            var jsonModel = JsonConvert.SerializeObject(scrappedModel);
+            var jsonModel = JsonSerializer.Serialize(scrappedModel,
+                new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+                });
 
             return Ok(jsonModel);
         }
