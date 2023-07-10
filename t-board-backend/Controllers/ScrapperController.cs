33f8bfdf2c3405188a19c8e194ce4bf3ce8f62d1
mbsagin <mbsagin@gmail.com>
2023-07-10 23:22:47 +0300

    MetaData scrapper

--- t-board-backend/Controllers/ScrapperController.cs
+++ t-board-backend/Controllers/ScrapperController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
 using System.Threading.Tasks;
+using t_board.Helpers;
 using t_board.Services.Contracts;
+using t_board_backend.Models.Scrapper;
 
 namespace t_board_backend.Controllers
 {
@@ -66,6 +71,45 @@ namespace t_board_backend.Controllers
                 });
 
             return Ok(jsonModel);
+        }
+
+        [HttpPost("scrapeMetaData")]
+        [Produces("application/json")]
+        public async Task<IActionResult> ScrapeMetaData(string url)
+        {
+            if (Validator.IsValidUri(url) is false) return BadRequest("URL is not valid. Content could not scrapped.");
+
+            var metaData = new MetaData();
+
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var titleNode = doc.DocumentNode.SelectSingleNode("/html/head/title");
+
+            if (titleNode != null)
+            {
+                metaData.Title = titleNode.InnerText;
+            }
+
+            var metaNodes = doc.DocumentNode.SelectNodes("/html/head/meta");
+            foreach (var metaNode in metaNodes)
+            {
+                var attributes = metaNode.GetAttributes().ToList();
+
+                if (attributes.Any(a => a.Name == "name" && a.Value == "description"))
+                {
+                    metaData.Description = attributes.FirstOrDefault(a => a.Name == "content")?.Value;
+                }
+
+                if (attributes.Any(a => a.Name == "property" && a.Value == "og:image"))
+                {
+                    metaData.ThumbnailURL = attributes.FirstOrDefault(a => a.Name == "content")?.Value;
+                }
+
+                attributes.ForEach(a => Console.WriteLine(a.Name + " " + a.Value));
+            }
+
+            return Ok(metaData);
         }
     }
 }
---
+++ t-board-backend/Models/Scrapper/MetaData.cs
@@ -0,0 +1,9 @@
+﻿namespace t_board_backend.Models.Scrapper
+{
+    public class MetaData
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailURL { get; set; }
+    }
+}
