f5a3e47987daa2ea2f1667cda3269f235285e491
mbsagin <mbsagin@gmail.com>
2023-07-10 23:06:08 +0300

    CompanyUser will return UserId

--- t-board-backend/Models/Company/Dto/CompanyUserDto.cs
+++ t-board-backend/Models/Company/Dto/CompanyUserDto.cs
@@ -4,10 +4,12 @@ namespace t_board_backend.Models.Company.Dto
 {
     public class CompanyUserDto
     {
+        public string UserId { get; set; }
         public int CompanyId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string Title { get; set; }
         public string AvatarURL { get; set; }
         public bool AccountLocked { get; set; }
