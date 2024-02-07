e8fad5f39bcc57ed5617682740af7f368d609a16
mbsagin <mbsagin@gmail.com>
2024-02-07 16:56:39 +0300

    Serilog Seq sink

--- t-board-backend/Middlewares/ExceptionMiddleware.cs
+++ t-board-backend/Middlewares/ExceptionMiddleware.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Serilog;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -14,15 +12,12 @@ namespace t_board_backend.Middlewares
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly IConfiguration Configuration;
         private readonly ILogger<ExceptionMiddleware> _logger;
 
         public ExceptionMiddleware(
             RequestDelegate next,
-            IConfiguration configuration,
             ILogger<ExceptionMiddleware> logger)
         {
-            Configuration = configuration;
             _logger = logger;
             _next = next;
         }
@@ -35,7 +30,8 @@ namespace t_board_backend.Middlewares
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Something went wrong: {ex}");
+                _logger.LogError("Internal Server Error: {@Exception}", ex);
+
                 await HandleExceptionAsync(httpContext, ex);
             }
         }
@@ -52,30 +48,6 @@ namespace t_board_backend.Middlewares
             };
 
             await context.Response.WriteAsync(JsonConvert.SerializeObject(response));
-
-            await LogError(exception);
-        }
-
-        private async Task LogError(object error)
-        {
-            var serverURL = Configuration["Seq:SERVER_URL"];
-            var apiKey = Configuration["Seq:API_KEY"];
-
-            Serilog.Log.Logger = new Serilog.LoggerConfiguration()
-                .WriteTo.Seq(serverURL, apiKey: apiKey)
-                .CreateLogger();
-
-            var serializerSetting = new JsonSerializerSettings()
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-            };
-
-            var serializedError = JsonConvert.SerializeObject(error, serializerSetting);
-
-            Serilog.Log.Error(serializedError);
-
-            await Serilog.Log.CloseAndFlushAsync();
         }
     }
 }
